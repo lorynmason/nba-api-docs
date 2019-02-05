a5b7fa78f3c5907aac6cb775682b734d98a6573c
Loryn Mason <masonloryn@gmail.com>
2019-02-05 14:18:12 -0700

    Setup files and component basics

---
+++ src/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul> 
+        <h4>API ENDPOINTS</h4>
+        <li>GET</li>
+        <li>POST</li>
+        <li>PATCH</li>
+        <li>DELETE</li>
+      </ul>
+      <h4>MORE INFO</h4>
+    </nav>
+  )
+}
+
+export default Nav
